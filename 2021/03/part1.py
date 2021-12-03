b577fa56aa06e324f3fb239d5d89305fa58118b5
Arash <47273556+arashnrim@users.noreply.github.com>
2021-12-03 14:25:10 +0800

    Complete Day 3

---
+++ 2021/03/part1.py
@@ -0,0 +1,28 @@
+try:
+    with open ("input.txt", "r") as fileContent:
+        diagnosticBits = [line.strip("\n") for line in fileContent.readlines()]
+except FileNotFoundError:
+    print("[!] The input file was not found. The program will not continue.")
+
+gamma, epsilon = "", ""
+
+for index in range(len(diagnosticBits[0])):
+    zeroes, ones = 0, 0
+    for diagnosticBit in diagnosticBits:
+        zeroes += 1 if diagnosticBit[index] == "0" else 0
+        ones += 1 if diagnosticBit[index] == "1" else 0
+    if ones > zeroes:
+        gamma += "1"
+        epsilon += "0"
+    elif zeroes > ones:
+        gamma += "0"
+        epsilon += "1"
+
+gammaDecimal, epsilonDecimal, index = 0, 0, 0
+
+for exponent in range(len(gamma) - 1, -1, -1):
+    gammaDecimal += int(gamma[index]) * (2 ** exponent)
+    epsilonDecimal += int(epsilon[index]) * (2 ** exponent)
+    index += 1
+
+print(gammaDecimal * epsilonDecimal)
---
+++ 2021/03/part2.py
@@ -0,0 +1,29 @@
+try:
+    with open ("input.txt", "r") as fileContent:
+        diagnosticBits = [line.strip("\n") for line in fileContent.readlines()]
+except FileNotFoundError:
+    print("[!] The input file was not found. The program will not continue.")
+
+def filterBits(remainingBits, bitColumn, searchingFor):
+    filteredBits = []
+
+    zeroes, ones = 0, 0
+    for diagnosticBit in remainingBits:
+        zeroes += 1 if diagnosticBit[bitColumn] == "0" else 0
+        ones += 1 if diagnosticBit[bitColumn] == "1" else 0
+    for diagnosticBit in remainingBits:
+        bitToFilter = ("0" if zeroes > ones else "1") if searchingFor == "oxygen" else ("0" if zeroes <= ones else "1")
+        if diagnosticBit[bitColumn] == bitToFilter: filteredBits.append(diagnosticBit)
+
+    return filteredBits[0] if len(filteredBits) == 1 else filterBits(filteredBits, bitColumn + 1, searchingFor)
+
+oxygenResult = filterBits(diagnosticBits, 0, "oxygen")
+co2Result = filterBits(diagnosticBits, 0, "C02")
+
+oxygenDecimal, co2Decimal, index = 0, 0, 0
+for exponent in range(len(oxygenResult) - 1, -1, -1):
+    oxygenDecimal += int(oxygenResult[index]) * (2 ** exponent)
+    co2Decimal += int(co2Result[index]) * (2 ** exponent)
+    index += 1
+
+print(oxygenDecimal * co2Decimal)
